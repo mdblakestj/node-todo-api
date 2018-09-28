948bf21fb7b59552f73014ef9a668b50a88683b4
Daniel schwartz <mdblakestj@gmail.com>
2018-09-28 17:28:56 -0400

    added password hashing

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -65,13 +66,33 @@ UserSchema.statics.findByToken = function (token) {
   } catch (e) {
     return Promise.reject();
   }
-  
+
   return User.findOne({
     _id: decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   })
 }
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err,salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        console.log(`hash is ${hash}`)
+        console.log(`user.password is ${user.password}`)
+        user.password = hash;
+        console.log(`user.password is ${user.password}`)
+        next();
+      })
+    })
+
+    
+  } else {
+    next();
+  }
+
+})
 
 var User = mongoose.model("User", UserSchema);
 
